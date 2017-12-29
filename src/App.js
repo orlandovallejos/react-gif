202a55c60e5c28a2c11d8bf5ebf0dbef78a00bd9
Orlando Vallejos <orlando@zlapps.com>
2017-12-29 15:21:28 -0500

    Basic html structure with AntDesign.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Layout, Menu, Icon } from 'antd';
 import './App.css';
+
+const { Header, Content, Footer, Sider } = Layout;
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Layout>
+        <Sider
+          breakpoint="lg"
+          collapsedWidth="0"
+          onCollapse={(collapsed, type) => { console.log(collapsed, type); }}
+        >
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1">
+              <Icon type="user" />
+              <span className="nav-text">Home</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="heart" />
+              <span className="nav-text">Favorites</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header style={{ background: '#fff', padding: 0 }} />
+          <Content style={{ margin: '24px 16px 0' }}>
+            <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+              content
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>
+            Ant Design ©2016 Created by Ant UED
+          </Footer>
+        </Layout>
+      </Layout>
     );
   }
 }
